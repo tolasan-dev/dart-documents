b5b7a10be353afa355f3746e3d7bb48bca36dc03
santola.developer@gmail.com <santola.developer@gmail.com>
2025-10-28 10:12:42 +0700

    init create project new of dart

---
+++ lap-json/my_json-app/bin/my_json_app.dart
@@ -0,0 +1,5 @@
+import 'package:my_json_app/my_json_app.dart' as my_json_app;
+
+void main(List<String> arguments) {
+  print('Hello world: ${my_json_app.calculate()}!');
+}
---
+++ lap-json/my_json-app/test/my_json_app_test.dart
@@ -0,0 +1,8 @@
+import 'package:my_json_app/my_json_app.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
